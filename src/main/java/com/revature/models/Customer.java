73e600aa23818ac1f0034e9a2b73de48bbd5be4a
Yuriy Koziy <yukoziy@gmail.com>
2022-06-18 15:16:43 -0400

    Added methods to get customer info and their account info

--- src/main/java/com/revature/models/Customer.java
+++ src/main/java/com/revature/models/Customer.java
@@ -1,5 +1,9 @@
 package com.revature.models;
 
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
 public class Customer extends User
 {
 	private Account account;
@@ -14,11 +18,53 @@ public class Customer extends User
 	
 	private String address;
 
+	public Customer()
+	{
+		account = new Account();
+	}
+	
+	public Customer(String firstName, String lastName, String userName, String phoneNumber, String address)
+	{
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		account = new Account();
+	}
+	
 	public Account getAccount()
 	{
 		return account;
 	}
-
+	
+	public String getCustomerInfo()
+	{
+		String info = "\n=======================================================\n";
+		info += "Name: " + firstName + " " + lastName + "\n";
+		info += "User Name: " + userName + "\n";
+		info += "Address: " + address + "\n";
+		info += "Phone Number: " + phoneNumber + "\n";
+		info += "Account Number: " + account.getNumber() + "\n";
+		info += "=======================================================\n";
+		return info;
+	}
+	
+	public String getAccountInfo()
+	{
+		Locale usa = new Locale("en", "US");
+		Currency dollars = Currency.getInstance(usa);
+		NumberFormat dollarFormat = NumberFormat.getCurrencyInstance(usa);
+		double balance = account.getBalance();
+		
+		String info = "\n=======================================================\n";
+		info += "Account Number: " + account.getNumber() + "\n";
+		info += "Account Balance: " + dollarFormat.format(balance) + "\n";
+		info += "=======================================================\n";
+		return info;		
+	}
+	
 	public void setAccount(Account account)
 	{
 		this.account = account;
@@ -62,11 +108,6 @@ public class Customer extends User
 	public void setPhoneNumber(String phoneNumber)
 	{
 		this.phoneNumber = phoneNumber;
-	}
-
-	public Customer()
-	{
-		account = new Account();
 	}
 
 	public double getAccountBalance()
--- src/test/java/com/revature/models/CustomerTest.java
+++ src/test/java/com/revature/models/CustomerTest.java
@@ -13,51 +13,73 @@ public class CustomerTest
 	@Before
 	public void resetCustomer()
 	{
-		Customer client = new Customer();
+		testCustomer = new Customer();
 		SequenceGenerator.setCounter(0L);
 	}
 	
 	@Test
 	public void customerShouldBeAbleToDebitAccount()
-	{
-		Customer client = new Customer();
+	{	
+		testCustomer.debitAccount(50.05);
 		
-		client.debitAccount(50.05);
-		
-		Assert.assertEquals(50.05, client.getAccountBalance(), 0.1);
+		Assert.assertEquals(50.05, testCustomer.getAccountBalance(), 0.1);
 	}
 	
 	@Test
 	public void customerShouldNotBeAbleToDebitnegativeAmount()
 	{
-		Customer client = new Customer();
+		testCustomer.debitAccount(-50.05);
 		
-		client.debitAccount(-50.05);
-		
-		Assert.assertNotEquals(-50.05, client.getAccountBalance(), 0.1);		
+		Assert.assertNotEquals(-50.05, testCustomer.getAccountBalance(), 0.1);		
 	}
 	
 	@Test
 	public void customerShouldBeAbleToCreditAccount()
 	{
-		Customer client = new Customer();
+		testCustomer.debitAccount(100.01);
 		
-		client.debitAccount(100.01);
+		testCustomer.creditAccount(45.35);
 		
-		client.creditAccount(45.35);
-		
-		Assert.assertEquals(54.66, client.getAccountBalance(), 0.1);
+		Assert.assertEquals(54.66, testCustomer.getAccountBalance(), 0.1);
 	}
 	
 	@Test
 	public void customerShouldNotBeAbleToOverdraft()
 	{
-		Customer client = new Customer();
+		testCustomer.debitAccount(100.01);
 		
-		client.debitAccount(100.01);
+		testCustomer.creditAccount(1000000);
 		
-		client.creditAccount(1000000);
+		Assert.assertEquals(100.01, testCustomer.getAccountBalance(), 0.1);		
+	}
+	
+	@Test
+	public void getCustomerInfoShouldReturnCustomerInfo()
+	{
+		String info = "\n=======================================================\n";
+		info += "Name: John Doe\n";
+		info += "User Name: jdoe45\n";
+		info += "Address: 404 Example Rd, New York, NY 10017\n";
+		info += "Phone Number: 646-911-0101\n";
+		info += "Account Number: 00000000000000001\n";
+		info += "=======================================================\n";
 		
-		Assert.assertEquals(100.01, client.getAccountBalance(), 0.1);		
+		testCustomer = new Customer("John", "Doe", "jdoe45", "646-911-0101", "404 Example Rd, New York, NY 10017");
+		
+		Assert.assertEquals(info, testCustomer.getCustomerInfo());
+	}
+	
+	@Test
+	public void getAccountInfoShouldReturnAccountInfo()
+	{
+		String info = "\n=======================================================\n";
+		info += "Account Number: 00000000000000001\n";
+		info += "Account Balance: $1,032.45\n";
+		info += "=======================================================\n";
+		
+		testCustomer = new Customer("John", "Doe", "jdoe45", "646-911-0101", "404 Example Rd, New York, NY 10017");
+		testCustomer.debitAccount(1032.45);
+		
+		Assert.assertEquals(info, testCustomer.getAccountInfo());
 	}
 }
