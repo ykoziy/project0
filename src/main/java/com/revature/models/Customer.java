0d4920c8280460de11443f06117607214ebcc6b3
Yuriy Koziy <yukoziy@gmail.com>
2022-06-18 14:49:10 -0400

    Added getters/setters for fields in Customer class

--- src/main/java/com/revature/models/Customer.java
+++ src/main/java/com/revature/models/Customer.java
@@ -3,6 +3,66 @@ package com.revature.models;
 public class Customer extends User
 {
 	private Account account;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String userName;
+	
+	private String phoneNumber;
+	
+	private String address;
+
+	public Account getAccount()
+	{
+		return account;
+	}
+
+	public void setAccount(Account account)
+	{
+		this.account = account;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+
+	public String getUserName()
+	{
+		return userName;
+	}
+
+	public void setUserName(String userName)
+	{
+		this.userName = userName;
+	}
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
 
 	public Customer()
 	{
@@ -28,5 +88,15 @@ public class Customer extends User
 		{
 			account.withdraw(amount);
 		}
+	}
+
+	public String getAddress()
+	{
+		return address;
+	}
+
+	public void setAddress(String address)
+	{
+		this.address = address;
 	}
 }
