2084dce35160b251fbf31b762ac3aecf2f194cb3
Yuriy Koziy <yukoziy@gmail.com>
2022-06-19 19:49:45 -0400

    working on Bank class

--- src/main/java/com/revature/models/Bank.java
+++ src/main/java/com/revature/models/Bank.java
@@ -1,6 +1,22 @@
 package com.revature.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank
 {
-
+	private Map<String, User> users;
+	private Map<String, Account> accounts;
+	
+	public Bank() 
+	{
+		users = new HashMap<>();
+		accounts = new HashMap<>();
+	}
+	
+	public Bank(Map<String, User> users, Map<String, Account> accounts)
+	{
+		this.users = users;
+		this.accounts = accounts;
+	}
 }
---
+++ src/test/java/com/revature/models/BankTest.java
@@ -0,0 +1,8 @@
+package com.revature.models;
+
+import org.junit.Test;
+
+public class BankTest
+{
+	
+}
