58b054503a70fc910b69e67223e928f88ff488a1
Yuriy Koziy <yukoziy@gmail.com>
2022-06-19 20:52:36 -0400

    Working on Bank class, ability to add users and accounts with ability to remove.

--- src/main/java/com/revature/models/Bank.java
+++ src/main/java/com/revature/models/Bank.java
@@ -19,4 +19,54 @@ public class Bank
 		this.users = users;
 		this.accounts = accounts;
 	}
+	
+	public void addCustomer(Customer customer)
+	{
+		users.put(customer.getUserName(), customer);
+	}
+	
+	public void removeCustomer(String userName)
+	{
+		users.remove(userName);
+	}
+	
+	public void addAccount(Account account)
+	{
+		accounts.put(account.getNumber(), account);
+	}
+	
+	public void removeAccount(String accountNumber)
+	{
+		accounts.remove(accountNumber);
+	}
+
+	public Map<String, User> getUsers()
+	{
+		return users;
+	}
+
+	public void setUsers(Map<String, User> users)
+	{
+		this.users = users;
+	}
+
+	public Map<String, Account> getAccounts()
+	{
+		return accounts;
+	}
+
+	public void setAccounts(Map<String, Account> accounts)
+	{
+		this.accounts = accounts;
+	}
+	
+	public User getUser(String userName) 
+	{
+		return users.get(userName);
+	}
+	
+	public Account getAccount(String accountNumber) 
+	{
+		return accounts.get(accountNumber);
+	}
 }
--- src/test/java/com/revature/models/BankTest.java
+++ src/test/java/com/revature/models/BankTest.java
@@ -1,8 +1,87 @@
 package com.revature.models;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.revature.util.SequenceGenerator;
 
 public class BankTest
 {
+	private Bank testBank;
 	
+	@Before
+	public void before()
+	{
+		testBank = new Bank();
+		SequenceGenerator.setCounter(0L);
+	}
+	
+	@Before
+	public void reset()
+	{
+		testBank = new Bank();
+		SequenceGenerator.setCounter(0L);
+	}
+	
+	@Test
+	public void bankShouldBeAbleToAddAUser()
+	{
+		Customer testCustomer = new Customer("John", "Doe", "jdoe45", "646-911-0101", "404 Example Rd, New York, NY 10017");
+		
+		testBank.addCustomer(testCustomer);
+		
+		User u = testBank.getUser("jdoe45");
+		
+		Assert.assertEquals("jdoe45", u.getUserName());
+	}
+	
+	@Test
+	public void bankShouldBeAbleToAddAnAccount()
+	{
+		Account acc = new Account();
+		
+		testBank.addAccount(acc);
+		
+		String accountNum = "00000000000000001";
+		
+		Account a = testBank.getAccount(accountNum);
+		
+		Assert.assertEquals(accountNum, a.getNumber());
+	}
+	
+	@Test
+	public void bankShouldBeAbleToRemoveUser()
+	{
+		Customer testCustomer = new Customer("John", "Doe", "jdoe45", "646-911-0101", "404 Example Rd, New York, NY 10017");
+		
+		testBank.addCustomer(testCustomer);
+		
+		Customer testCustomer2 = new Customer("Joe", "Doe", "jdoe55", "646-911-0209", "403 Example Rd, New York, NY 10017");
+		
+		testBank.addCustomer(testCustomer2);
+		
+		testBank.removeCustomer("jdoe45");
+		
+		Assert.assertNull(testBank.getUser("jdoe45"));
+		Assert.assertEquals("jdoe55", testBank.getUser("jdoe55").getUserName());
+	}
+	
+	@Test
+	public void bankShouldBeAbleToRemoveAccount()
+	{
+		Account acc = new Account();
+		Account acc2 = new Account();
+		
+		testBank.addAccount(acc);
+		testBank.addAccount(acc2);
+		
+		String accountNum = "00000000000000001";
+		String accountNum2 = "00000000000000002";
+		
+		testBank.removeAccount(accountNum2);
+		
+		Assert.assertNull(testBank.getAccount(accountNum2));
+		Assert.assertEquals(accountNum, testBank.getAccount(accountNum).getNumber());
+	}
 }
