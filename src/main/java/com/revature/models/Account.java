80ff7236f86e22f5066f605278b7432925a7e9fb
Yuriy Koziy <yukoziy@gmail.com>
2022-06-22 16:46:30 -0400

    Fixed Account class

--- src/main/java/com/revature/models/Account.java
+++ src/main/java/com/revature/models/Account.java
@@ -1,52 +1,80 @@
 package com.revature.models;
 
 import java.math.BigDecimal;
-import com.revature.util.SequenceGenerator;
 
 public class Account
 {
+	private long id;
+	private long ownerId;
 	private BigDecimal balance;
-	
-	private String number;
+	private String status;
 	
 	private static final int NUMBER_LENGTH = 17;
 	
-	public Account() {
-		balance = new BigDecimal(0.0);
-		number = generateAccountNumber();
+	public Account() 
+	{
+		super();
 	}
 
-	public void deposit(double amount)
+	public Account(long id, long ownerId, BigDecimal balance, String status)
 	{
-		balance = balance.add(BigDecimal.valueOf(amount));
+		super();
+		this.id = id;
+		this.ownerId = ownerId;
+		this.balance = balance;
+		this.status = status;
 	}
 	
-	public void withdraw(double amount)
+	public void setBalance(BigDecimal balance)
 	{
-		balance = balance.subtract(BigDecimal.valueOf(amount));
+		this.balance = balance;
 	}
 	
 	public double getBalance()
 	{
 		return balance.doubleValue();
 	}
-
-	public String getNumber()
-	{
-		return number;
-	}
 	
-	private String generateAccountNumber() {
-		long accountCount = SequenceGenerator.getNext();
+	public long getId()
+	{
+		return id;
+	}
+
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+
+	public long getOwnerId()
+	{
+		return ownerId;
+	}
+
+	public void setOwnerId(long ownerId)
+	{
+		this.ownerId = ownerId;
+	}
+
+	public String getStatus()
+	{
+		return status;
+	}
+
+	public void setStatus(String status)
+	{
+		this.status = status;
+	}
+
+	public String getAccountNumber() {
 		StringBuffer sb = new StringBuffer();
-		int padZeroCount = NUMBER_LENGTH - Long.toString(accountCount).length();
+		String id = Long.toString(this.id);
+		int padZeroCount = NUMBER_LENGTH - id.length();
 		
 		for (int i = 0; i < padZeroCount; i++) {
 			sb.append("0");
 		}
 		
-		sb.append(accountCount);
+		sb.append(id);
 		return sb.toString();
 	}
-	
 }
--- src/test/java/com/revature/models/AccountTest.java
+++ src/test/java/com/revature/models/AccountTest.java
@@ -1,10 +1,10 @@
 package com.revature.models;
 
+import java.math.BigDecimal;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.revature.util.SequenceGenerator;
 
 public class AccountTest
 {
@@ -13,53 +13,12 @@ public class AccountTest
 	@Before
 	public void setUpAccount()
 	{
-		testAccount = new Account();
-		SequenceGenerator.setCounter(0);
-	}
-
-	@Test
-	public void depositShouldIncreateBalance()
-	{
-		testAccount.deposit(100.18);
-		
-		Assert.assertEquals(100.18, testAccount.getBalance(), 0.1);
+		testAccount = new Account(1, 1, new BigDecimal(100.04), "active");
 	}
 	
 	@Test
-	public void withdrawShouldDecreaseBalance()
+	public void shouldGenerateValidAccountNumber()
 	{
-		
-		// if we use double for balance 2.15 i 1.10 would return 1.0499999999999998
-		// for banking application we want exact calculations
-		testAccount.deposit(2.15);
-		
-		testAccount.withdraw(1.10);
-		
-		Assert.assertEquals(1.05, testAccount.getBalance(), 0.1);
-	}
-	
-	@Test
-	public void creatingAccountShouldGenerateValidAccountNumber()
-	{
-		// valid account number:
-		// 17 long, contains only digits, in needed padded with zeros
-		
-		Account newAccount = new Account();
-		
-		Assert.assertEquals("00000000000000001", newAccount.getNumber());
-		
-	}
-	
-	@Test
-	public void creatingThreeAcountShouldGenerateCorrectAccountNumber()
-	{
-		Account acc1 = new Account();
-		Account acc2 = new Account();
-		Account acc3 = new Account();
-		
-		Assert.assertEquals("00000000000000001", acc1.getNumber());
-		Assert.assertEquals("00000000000000002", acc2.getNumber());
-		Assert.assertEquals("00000000000000003", acc3.getNumber());
-	}
-		
+		Assert.assertEquals("00000000000000001", testAccount.getAccountNumber());	
+	}	
 }
