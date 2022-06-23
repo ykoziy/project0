f812eec3042dbe3f67e5e15b672250c0af41d611
Yuriy Koziy <yukoziy@gmail.com>
2022-06-23 13:58:24 -0400

    fixes enum

--- src/main/java/com/revature/enums/UserRole.java
+++ src/main/java/com/revature/enums/UserRole.java
@@ -2,20 +2,7 @@ package com.revature.enums;
 
 public enum UserRole
 {
-	customer("customer"),
-	admin("admin"),
-	employee("employee");
-	
-	private final String text;
-
-	private UserRole(String text)
-	{
-		this.text = text;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return text;
-	}
+	customer,
+	employee,
+	admin
 }
