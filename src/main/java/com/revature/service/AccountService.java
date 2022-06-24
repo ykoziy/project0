5b95dccb2831d20dd3bdc314252cc9aa8f1c69a4
Yuriy Koziy <yukoziy@gmail.com>
2022-06-24 18:26:14 -0400

    Fineshed, createAccountForPerson untested

--- src/main/java/com/revature/service/AccountService.java
+++ src/main/java/com/revature/service/AccountService.java
@@ -4,11 +4,43 @@ import java.util.List;
 
 import com.revature.dao.AccountDao;
 import com.revature.dao.PsqlAccountDao;
+import com.revature.exceptions.RegisterPersonFailedException;
 import com.revature.models.Account;
+import com.revature.models.Address;
+import com.revature.models.Person;
 
 public class AccountService
 {
 	private AccountDao adao = new PsqlAccountDao();
+	
+	public Account createAccountForPerson(Person person, Account account)
+	{
+		if (person.getId() < 1) 
+		{
+			throw new RegisterPersonFailedException("Account not valid to register because person ID was invalid");
+		}
+		
+		if (account.getId() != 0) 
+		{
+			throw new RegisterPersonFailedException("Account not valid to register because account ID is not 0");
+		}
+		
+		if (account.getOwnerId() != person.getId()) 
+		{
+			throw new RegisterPersonFailedException("Account owner ID does not match person ID");
+		}
+		
+		long newAccountIndex = adao.add(person.getId(), account);
+		
+		if (newAccountIndex != -1 && newAccountIndex != account.getId()) 
+		{
+			account.setId(newAccountIndex);
+		} else {
+			throw new RegisterPersonFailedException("Account ID was either -1 or did not change after insertion");
+		}
+
+		return account;
+	}
 
 	public boolean deposit(int accountId)
 	{
