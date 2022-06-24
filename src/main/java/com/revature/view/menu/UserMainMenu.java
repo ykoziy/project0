65fda0b300f036f7fe20f83b2a2b5a6d403252b4
Yuriy Koziy <yukoziy@gmail.com>
2022-06-24 17:14:46 -0400

    Added stub class for UserMainMenu

---
+++ src/main/java/com/revature/view/menu/UserMainMenu.java
@@ -0,0 +1,10 @@
+package com.revature.view.menu;
+
+public class UserMainMenu
+{
+	// Customer should be able to deposit, withdraw, transfer (with validation), view accounts, view personal information, view account information
+	
+	// (for all customers) Employee view account information, approve/deny open applications for accounts
+	
+	// Admin, view/edit all accounts, approve/deny accounts, withdrawing, depositing, transferring from ALL accounts, canceling accounts
+}
