16a6c41a5d3e36b68e1201367a57494ac55f387d
Yuriy Koziy <yukoziy@gmail.com>
2022-06-28 15:48:20 -0400

    Added PasswordSecurity, to hash passwords using salted SHA512, and compare them

---
+++ src/main/java/com/revature/util/PasswordSecurity.java
@@ -0,0 +1,68 @@
+package com.revature.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class PasswordSecurity
+{
+	public static boolean comparePassword(String hashedPassword, String password)
+	{
+		String[] array = hashedPassword.split("\\.");
+		String hash = hashPassword(password, hexToByte(array[1]));
+		if (hash.equals(array[0]))
+		{
+			return true;
+		}
+		return false;
+	}
+
+	public static String hashPassword(String passwordToHash, byte[] salt)
+	{
+		String generatedPassword = null;
+		try
+		{
+			MessageDigest md = MessageDigest.getInstance("SHA-512");
+			// add salt
+			md.update(salt);
+			// digest string to get a byte array
+			byte[] bytes = md.digest(passwordToHash.getBytes());
+			// convert byte array to HEX string
+			generatedPassword = byteToHex(bytes);
+		} catch (NoSuchAlgorithmException e)
+		{
+			e.printStackTrace();
+		}
+		return generatedPassword;
+	}
+	
+	private static String byteToHex(byte[] bytes)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (byte b : bytes)
+		{
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
+	}
+	
+	private static byte[] hexToByte(String hex)
+	{
+		byte[] bytes = new byte[16];
+		String h = "";
+		for (int i = 0; i < bytes.length; i++)
+		{
+			h = hex.substring(2 * i, 2 * i + 2);
+			bytes[i] = (byte) Integer.parseInt(h, 16);
+		}
+		return bytes;
+	}
+
+	public static byte[] salt() throws NoSuchAlgorithmException
+	{
+		SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+		byte[] salt = new byte[16];
+		sr.nextBytes(salt);
+		return salt;
+	}
+}
